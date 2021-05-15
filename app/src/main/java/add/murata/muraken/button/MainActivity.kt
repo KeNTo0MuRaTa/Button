85a46fdb0db1b8e77252d45aa35f9799a6194757
kento <kento010610@icloud.com>
2021-05-15 11:33:47 +0900

    Button

---
+++ app/src/main/java/add/murata/muraken/button/MainActivity.kt
@@ -0,0 +1,22 @@
+package add.murata.muraken.button
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+
+        }
+        fun click(view : View){
+
+        }
+
+    }
+}
